44b6e8fdd9ac6c4e84eb03418864dad4ce7889e9
unknown <sstrzalecka@gmail.com>
2023-05-13 21:56:47 +0200

    sklep

---
+++ products.js
@@ -0,0 +1,12 @@
+const products =[
+    {
+        id: 0,
+        name: "Iphone 11",
+        description: '',
+        category: "Iphone",
+        price: 2999.99,
+        image:"./image/iphone11.png"
+        sale: true,
+        saleAmount: 200,
+    }
+]
